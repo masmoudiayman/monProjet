fa2347ec974223e71fd75d135740fd8f3490dea6
masmoudiayman <aymanmasmoudi7@gmail.com>
2021-10-23 10:47:13 +0100

    23/09/2021

---
+++ src/Controller/LukyController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LukyController extends AbstractController
+{
+    #[Route('/luky', name: 'luky')]
+    public function index(): Response
+    {
+        return $this->render('luky/index.html.twig', [
+            'controller_name' => 'LukyController',
+        ]);
+    }
+
+    #[Route('/number/{max}', name: 'number')]
+    public function number(int $max): Response 
+    {  
+        $number = random_int(0,$max);
+
+        //return new Response("<h1>Lucky number : {$number} </h1>");
+        return $this->render('luky/number.html.twig', [
+            'x' => $number,
+        ]);
+
+    }
+}
